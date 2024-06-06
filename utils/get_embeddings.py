72cc3e2651abbd8c94631f688af544bb436cac95
RasButAss <samarth3tankasali@gmail.com>
2024-06-06 16:28:33 +0900

    Some local merge conflicts.

--- utils/get_embeddings.py
+++ utils/get_embeddings.py
@@ -9,9 +9,23 @@ def get_embedding(text, embedding_model="text-embedding3"):
     azure_deployment=embedding_model
   )
   response = embeddings.embed_query(text)
-  print(f"Embedding model out: {response[0]}")
+  print(f"Embedding model out (OpenAI): {response[0]}")
 
   return response
 
+
+#e5 embeddings
+def get_e5_embedding(text):
+  text = text.replace("\n", " ")
+  device = torch.device('cuda' if  torch.cuda.is_available() else 'cpu')
+  print(device)
+  start=time.time()
+  model = SentenceTransformer('intfloat/e5-small').to(device)
+  with torch.no_grad():
+    embeddings = model.encode(text)
+  print(f"Time taken to get embeddings (e5): {time.time()-start}")
+  return embeddings
+
+#e5 embeddings
 if __name__ == "__main__":
   print(get_embedding("What is the meaning of life?"))
